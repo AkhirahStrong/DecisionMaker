5fdf3204a966fbe62f163dacda734322dcf7add4
Akhirahstrong <codeclubdetroit@gmail.com>
2022-04-03 15:28:36 -0400

    First Decition Maker project commit

---
+++ Grades/src/Grades.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Grades {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Please enter a numerical grade ");
+		int grade = in.nestInt(); 
+
+	}
+
+}
